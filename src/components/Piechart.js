c10b36ad92d1fc9a2182243a7be04a9c42531805
Aspartame-s <327127960@qq.com>
2022-04-05 00:26:07 +0800

    添加获取piechartdata算法

--- src/components/Piechart.js
+++ src/components/Piechart.js
@@ -1,8 +1,11 @@
 import React from 'react'
-import { ResponsiveContainer, PieChart, Pie, Legend, Tooltip } from "recharts";
+import { ResponsiveContainer, PieChart, Pie, Legend, Tooltip, Cell } from "recharts";
+import { Colors } from '../utility';
+
+const ColorsArr = Object.keys(Colors).map(key => Colors[key])
 
 const PieCharts = ({ pieChartData, title }) => {
-  if(pieChartData.length === 0) {
+  if (pieChartData.length === 0) {
     return <h3 className="text-center mt-3">{title}还没有任何数据</h3>
   }
   return (
@@ -14,12 +17,16 @@ const PieCharts = ({ pieChartData, title }) => {
             dataKey="value"
             isAnimationActive={false}
             data={pieChartData}
-            cx={200}
-            cy={200}
+            cx='50%' 
+            cy='50%'
             outerRadius={80}
             fill="#8884d8"
             label
-          />
+          >
+            {
+              pieChartData.map((entry, index) => <Cell key={index} fill={ColorsArr[index % ColorsArr.length]} />)
+            }
+          </Pie>
           <Tooltip />
         </PieChart>
       </ResponsiveContainer>
--- src/utility.js
+++ src/utility.js
@@ -58,3 +58,13 @@ export const getPieChartData = (data, type) => {
     })
     return pieChartData
 }
+
+export const Colors = {
+    blue: '#347eff',
+    deepBlue: '#61dafb',
+    green: '#28a745',
+    red: '#dc3545',
+    gray: '#555',
+    lightGray: '#efefef',
+    white: '#fff',
+  }
